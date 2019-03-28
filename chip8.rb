2bab43cceaf741349e1890a00d8eecc873c66450
Cezar Azevedo de Faveri <cazevedodefaveri@gmail.com>
2019-03-28 00:03:50 -0300

    More operations

--- chip8.rb
+++ chip8.rb
@@ -4,7 +4,7 @@ module Chip8
 			@memory = Memory.new(rom)
 			@cpu = CPU.new
 		end
-		
+
 		def run_next_cycle
 			@cpu.execute(@memory)
 		end
@@ -12,7 +12,7 @@ module Chip8
 
 	class Memory
 		INITIAL_MEMORY = 0x200
-	
+
 		CHARACTER_BYTES = [
 			[0xF0, 0x90, 0x90, 0x90, 0xF0], # "0"
 			[0x20, 0x60, 0x20, 0x20, 0x70], # "1"
@@ -32,24 +32,24 @@ module Chip8
 			[0xF0, 0x80, 0xF0, 0x80, 0x80]  # "F"
 		]
 
-		attr_accessor :V, :I, :gfx
+		attr_accessor :V, :I, :gfx, :stack
 
 		def initialize(rom)
 			@memory = Array.new(0x1000, 0x0)
 			rom.each_with_index do |opcode, i|
 				@memory[INITIAL_MEMORY + i] = opcode
 			end
-		
+
 			@current_address = INITIAL_MEMORY
 
 			self.V = Array.new(0x10, 0x0)
 			self.I = 0
 			self.gfx = Array.new(0x40, 0x0) {Array.new(0x20, 0x0)}
+			self.stack = Array.new
 
 			CHARACTER_BYTES.each_with_index do |character, ch_index|
-				puts "#{@memory}"
 				character.each_with_index do |byte, bt_index|
-					@memory[(5 * ch_index) + bt_index] = byte 
+					@memory[(5 * ch_index) + bt_index] = byte
 				end
 			end
 		end
@@ -60,8 +60,16 @@ module Chip8
 			opcode
 		end
 
+		def pc=(address)
+			@current_address = address
+		end
+
+		def pc
+			@current_address
+		end
+
 		def [](address)
-		  @memory[address]	
+		  @memory[address]
 		end
 
 		def []=(address, value)
@@ -76,8 +84,29 @@ module Chip8
 	class CPU
 		def execute(memory)
 			opcode = memory.current_opcode
-		
+
 			case (opcode & 0xF000)
+			when 0x0000
+				case (opcode & 0x00FF)
+				when 0x00EE
+					puts "Current: "
+					puts "pc: #{memory.pc}"
+					puts "stack: #{memory.stack}"
+					memory.pc = memory.stack.pop
+					puts "Final: "
+					puts "pc: #{memory.pc}"
+					puts "stack: #{memory.stack}"
+				else
+					raise "#{opcode.to_s(16)} not implemented yet!"
+				end
+			when 0x2000 # 2nnn - CALL addr
+				# Call subroutine at nnn.
+				# The interpreter increments the stack pointer,
+				# then puts the current PC on the top of the stack.
+				# The PC is then set to nnn.
+				memory.stack.push(memory.pc)
+
+				memory.pc = (opcode & 0x0FFF)
 			when 0x6000 # 6xkk - LD Vx, byte
 				# Set Vx = kk.
 				x = (opcode & 0x0F00) >> 8
@@ -85,6 +114,14 @@ module Chip8
 				memory.V[x] = kk
 
  				memory.step!
+			when 0x7000 # 7xkk - ADD Vx, byte
+				# Set Vx = Vx + kk.
+				x = (opcode & 0x0F00) >> 8
+				kk = (opcode & 0x00FF)
+
+				memory.V[x] += kk
+
+				memory.step!
 			when 0xA000 # Annn - LD I, addr
 				# Set I = nnn.
 				nnn = (opcode & 0x0FFF)
@@ -97,13 +134,41 @@ module Chip8
 				n = (opcode & 0x000F)
 				x = memory.V[(opcode & 0x0F00) >> 8]
 				y = memory.V[(opcode & 0x00F0) >> 4]
-					
+
 				n.times do |index|
 					sprite = memory[memory.I + index]
 					memory.gfx[x][y + index] ^= sprite
 				end
 
 				memory.step!
+			when 0xF000
+				case (opcode & 0x00FF)
+				when 0x0029 # Fx29 - LD F, Vx
+					# Set I = location of sprite for digit Vx.
+					x = (opcode & 0x0F00) >> 8
+					memory.I = memory[x * 5]
+
+					memory.step!
+				when 0x0033 # Fx33 - LD B, Vx
+					# Store BCD representation of Vx in memory locations
+					# I, I+1, and I+2.
+					x = (opcode & 0x0F00) >> 8
+					memory[memory.I] = x / 100
+					memory[memory.I + 1] = (x % 100) / 100
+					memory[memory.I + 2] = x % 10
+
+					memory.step!
+				when 0x0065 # Fx65 - LD Vx, [I]
+					# The interpreter reads values from memory starting at location I
+					# into registers V0 through Vx.
+					x = (opcode & 0x0F00) >> 8
+					x.times do |index|
+						memory.V[index] = memory[memory.I + index]
+					end
+					memory.step!
+				else
+					raise "#{opcode.to_s(16)} not implemented yet!"
+				end
 			else
 				raise "#{opcode.to_s(16)} not implemented yet!"
 			end
